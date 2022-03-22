06ee69be4f7ab2e72ea004af0c059311aa9e2f5d
lee9805 <dlthwjd1161@naver.com>
2022-03-22 09:19:39 +0900

    hello

---
+++ HelloKorea/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class HelloKorea {
+
+	public static void main(String[] args) {
+		System.out.println("æ»≥Á«œººø‰");
+		System.out.println(30+40);
+		
+
+	}
+
+}
