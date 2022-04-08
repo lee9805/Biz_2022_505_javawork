61f5409d8ead32a1efb7b150b02f35b161fdee85
lee9805 <dlthwjd1161@naver.com>
2022-04-08 11:52:44 +0900

    Java_Arrays 생성

---
+++ Java_30_Arrays/src/com/callor/arrays/controller/ControllerV3.java
@@ -0,0 +1,14 @@
+package com.callor.arrays.controller;
+
+import com.callor.arrays.service.ServiceV3;
+
+public class ControllerV3 {
+
+	public static void main(String[] args) {
+
+		ServiceV3 v3 = new ServiceV3();
+		v3.viewKor3();
+		v3.viewEng3();
+	}
+
+}
