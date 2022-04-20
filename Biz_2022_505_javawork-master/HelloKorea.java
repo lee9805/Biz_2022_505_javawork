1e022f91b9c52fdd479074dad072a64ffc03d8c3
lee9805 <dlthwjd1161@naver.com>
2022-04-20 18:46:01 +0900

    home

--- Biz_2022_505_javawork-master/HelloKorea.java
+++
@@ -1,8 +0,0 @@
-/*
-*/
-public class HelloKorea {
-	public static void main(String[] args){
-	   System.out.println("HelloKorea");
-	}
-
-}
--- Biz_2022_505_javawork-master/Java_20_Control_04/src/com/callor/service/ServiceV2.java
+++
@@ -1,9 +0,0 @@
-package com.callor.service;
-
-public class ServiceV2 {
-	
-	public void guguDan() {
-		
-	}
-
-}
