495c5ccdabf3d36095be183567065b8cb0934c73
lee9805 <dlthwjd1161@naver.com>
2022-04-27 12:59:38 +0900

    prime 내용  변경

--- Java_50_App_03/src/com/callor/app/exec/Exec4A.java
+++ Java_50_App_03/src/com/callor/app/exec/Exec4A.java
@@ -6,19 +6,19 @@ public class Exec4A {
 	 * 1 ~ 100까지 임의 수를 생성하고 그 수가 소수 (prime number)인지 판별하시오
 	 */
 	public static void main(String[] args) {
-		
-		int count = 0;
+
+		int n = 0;
 		int num = (int) (Math.random() * 100) + 1;// num의 변수에 랜덤 숫자 저장
-		for( int i = 2 ; i  < num ; i++) {
-			if(num % i ==0) {
-				count++;
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				n++;
 				break;
 			}
 		}
-		if(count == 0) {
+		if (n == 0) {
 			System.out.println(num + "은(는) 소수 이다");
-		}else {
-			System.out.println(num +"은(는) 소수가 아니다");
+		} else {
+			System.out.println(num + "은(는) 소수가 아니다");
 		}
 	}
 }
